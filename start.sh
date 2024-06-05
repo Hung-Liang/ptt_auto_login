19590117d5243d4376431f5e46a004e71f20dc8a
HM <zxjonathanzx@gmail.com>
2024-06-05 12:24:57 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo docker stop ptt_auto_login
-echo y|docker system prune
+echo y|sudo docker system prune
 sudo docker build --no-cache -t ptt_auto_login .
 sudo docker run --restart always --name ptt_auto_login -d ptt_auto_login
